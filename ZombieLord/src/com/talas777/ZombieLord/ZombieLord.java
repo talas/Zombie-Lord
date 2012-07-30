4fb16686897dfd8fd87951e86f42afac46bb893f
Talas <talas777@gmail.com>
2012-07-30 16:45:13 +0200

    Added more collision boundaries for the first town

--- ZombieLord/src/com/talas777/ZombieLord/ZombieLord.java
+++ ZombieLord/src/com/talas777/ZombieLord/ZombieLord.java
@@ -62,6 +62,7 @@ public class ZombieLord implements ApplicationListener {
 	
 	private World world;
 	private Body jumper;
+	private Body pointer;
 	
 	private Box2DDebugRenderer debugRenderer;
 	
@@ -218,7 +219,7 @@ public class ZombieLord implements ApplicationListener {
 		//loadCombat(4,area);
 
 		// uncomment to enable box2d debug render mode, MAJOR SLOWDOWN! 
-		//debugRenderer = new Box2DDebugRenderer();
+		debugRenderer = new Box2DDebugRenderer();
 		
 		
 	}
@@ -343,16 +344,39 @@ public class ZombieLord implements ApplicationListener {
 		jumperFixtureDef.shape = jumperShape;
 		jumperFixtureDef.density = 0.1f;
 		jumperFixtureDef.friction = 1.0f;
+		//jumperFixtureDef.filter.groupIndex = -2;
+		//jumperFixtureDef.filter.categoryBits = 2;
+		//jumperFixtureDef.filter.maskBits = 2;
 
 		jumper.createFixture(jumperFixtureDef);
 		jumper.setLinearDamping(9.0f);
 		jumperShape.dispose();
 		
+		BodyDef jt = new BodyDef();
+		jt.type = BodyDef.BodyType.DynamicBody;
+		jt.position.set(posx/PIXELS_PER_METER, posy/PIXELS_PER_METER);
+		
+		
+		pointer = world.createBody( jt  );
+		
+		PolygonShape js = new PolygonShape();
+		js.setAsBox(0.01f, 0.01f);
+		
+		FixtureDef jf = new FixtureDef();
+		jf.shape = js;
+		//jf.filter.groupIndex = -3;
+		//jf.filter.categoryBits = 2;
+		jf.filter.maskBits = 2;
+		
+		
+		pointer.createFixture(jf);
+		
+		
 		if(levelCode == 0 || levelCode == 3){
 			BodyDef groundBodyDef = new BodyDef();
 			groundBodyDef.type = BodyDef.BodyType.StaticBody;
 			Body groundBody = world.createBody(groundBodyDef);
-			{
+			{ // church
 				ChainShape environmentShape = new ChainShape();
 				
 				Vector2[] vertices = vectorize(
@@ -362,6 +386,151 @@ public class ZombieLord implements ApplicationListener {
 				vertices[1] = new Vector2(1473/PIXELS_PER_METER,480/PIXELS_PER_METER);
 				vertices[2] = new Vector2(1985/PIXELS_PER_METER,480/PIXELS_PER_METER);
 				vertices[3] = new Vector2(1985/PIXELS_PER_METER,322/PIXELS_PER_METER);*/
+	
+				environmentShape.createLoop(vertices);
+				groundBody.createFixture(environmentShape, 0);
+				environmentShape.dispose();
+			}
+			{ // gravestone 1
+				ChainShape environmentShape = new ChainShape();
+				
+				Vector2[] vertices = vectorize(
+						new float[]{1898,1898,1941,1941},
+						new float[]{225,265,265,225});
+				/*vertices[0] = new Vector2(1473/PIXELS_PER_METER,322/PIXELS_PER_METER);
+				vertices[1] = new Vector2(1473/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[2] = new Vector2(1985/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[3] = new Vector2(1985/PIXELS_PER_METER,322/PIXELS_PER_METER);*/
+	
+				environmentShape.createLoop(vertices);
+				groundBody.createFixture(environmentShape, 0);
+				environmentShape.dispose();
+			}
+			{ // gravestone 2
+				ChainShape environmentShape = new ChainShape();
+				
+				Vector2[] vertices = vectorize(
+						new float[]{1953,1953,1985,1985},
+						new float[]{288,322,322,288});
+				/*vertices[0] = new Vector2(1473/PIXELS_PER_METER,322/PIXELS_PER_METER);
+				vertices[1] = new Vector2(1473/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[2] = new Vector2(1985/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[3] = new Vector2(1985/PIXELS_PER_METER,322/PIXELS_PER_METER);*/
+	
+				environmentShape.createLoop(vertices);
+				groundBody.createFixture(environmentShape, 0);
+				environmentShape.dispose();
+			}
+			{ // gravestones 3
+				ChainShape environmentShape = new ChainShape();
+				
+				Vector2[] vertices = vectorize(
+						new float[]{1989,1989,2106,2106},
+						new float[]{226,254,254,226});
+				/*vertices[0] = new Vector2(1473/PIXELS_PER_METER,322/PIXELS_PER_METER);
+				vertices[1] = new Vector2(1473/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[2] = new Vector2(1985/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[3] = new Vector2(1985/PIXELS_PER_METER,322/PIXELS_PER_METER);*/
+	
+				environmentShape.createLoop(vertices);
+				groundBody.createFixture(environmentShape, 0);
+				environmentShape.dispose();
+			}
+			{ // gravestones 4
+				ChainShape environmentShape = new ChainShape();
+				
+				Vector2[] vertices = vectorize(
+						new float[]{2026,2026,2043,2043,2047,2047,2107,2107,2069,2069},
+						new float[]{289 , 329, 340, 350, 350, 382, 382, 352, 352, 289});
+				/*vertices[0] = new Vector2(1473/PIXELS_PER_METER,322/PIXELS_PER_METER);
+				vertices[1] = new Vector2(1473/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[2] = new Vector2(1985/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[3] = new Vector2(1985/PIXELS_PER_METER,322/PIXELS_PER_METER);*/
+	
+				environmentShape.createLoop(vertices);
+				groundBody.createFixture(environmentShape, 0);
+				environmentShape.dispose();
+			}
+			{ // gravestones 5
+				ChainShape environmentShape = new ChainShape();
+				
+				Vector2[] vertices = vectorize(
+						new float[]{1985,1985,2008,2000},
+						new float[]{353 , 382, 382, 353});
+				/*vertices[0] = new Vector2(1473/PIXELS_PER_METER,322/PIXELS_PER_METER);
+				vertices[1] = new Vector2(1473/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[2] = new Vector2(1985/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[3] = new Vector2(1985/PIXELS_PER_METER,322/PIXELS_PER_METER);*/
+	
+				environmentShape.createLoop(vertices);
+				groundBody.createFixture(environmentShape, 0);
+				environmentShape.dispose();
+			}
+			{ // gravestone 6
+				ChainShape environmentShape = new ChainShape();
+				
+				Vector2[] vertices = vectorize(
+						new float[]{2110,2110,2129,2138,2144},
+						new float[]{288 , 352, 352, 345, 290});
+				/*vertices[0] = new Vector2(1473/PIXELS_PER_METER,322/PIXELS_PER_METER);
+				vertices[1] = new Vector2(1473/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[2] = new Vector2(1985/PIXELS_PER_METER,480/PIXELS_PER_METER);
+				vertices[3] = new Vector2(1985/PIXELS_PER_METER,322/PIXELS_PER_METER);*/
+	
+				environmentShape.createLoop(vertices);
+				groundBody.createFixture(environmentShape, 0);
+				environmentShape.dispose();
+			}
+			{ // farm fence (only outer)
+				ChainShape environmentShape = new ChainShape();
+				
+				Vector2[] vertices = vectorize(
+						new float[]{2445,2445,2476,2476,2975,2975,2931,2931,3040,3040,3008,3008,3085,3085, 3200},
+						new float[]{1   , 734, 734, 797, 797, 774, 774, 734, 734, 776, 776, 797, 797, 828, 828});
+	
+				environmentShape.createChain(vertices); // not a loop!
+				groundBody.createFixture(environmentShape, 0);
+				environmentShape.dispose();
+			}
+			
+			{ // pond (near square)
+				ChainShape environmentShape = new ChainShape();
+				
+
+				Vector2[] vertices = vectorize(
+						new float[]{1906,1906,1961,1971,2026,2026,2002,2002,2022,2047,2150,2162,2162,2217,2224,2274,2289,2315,2324,2380,2380,2305,2293,2280
+								,2200,2190,2159,2159,2135,2122,2002,2002,1970,1970},
+						new float[]{1048,1135,1135,1108,1108,1127,1151,1194,1194,1232,1232,1254,1293,1293,1172,1171,1143,1136,1108,1101,1010,1010,1000,979,979
+								,1001,1001,952,939,883,883,935,953,1033});
+	
+				environmentShape.createLoop(vertices);
+				groundBody.createFixture(environmentShape, 0);
+				environmentShape.dispose();
+			}
+			
+			{ // top houses and fence
+				ChainShape environmentShape = new ChainShape();
+				
+
+				Vector2[] vertices = vectorize(
+						new float[]{3206,3138,3138,3099,3099,2946,2946,2908,2908,2880,2880,2754,2754,2717,2717,2689,2689,
+								2654,2654,2563,2563,2348,2348,2304,2304,2207,2207,2112,2112,2077,2077,2020,2016,1922,1922,1953,
+								2269,2303,2303,2403,2403,2369,2388,2479},
+						new float[]{1409,1409,1446,1446,1408,1408,1439,1439,1409,1409,1440,1440,1505,1505,1442,1442,1477,
+								1477,1440,1440,1512,1512,1799,1799,1666,1666,1536,1536,1569,1569,1536,1536,1730,1730,1974,1983,
+								1983,1974,1822,1822,1859,1874,1925,1948});
+	
+				environmentShape.createLoop(vertices);
+				groundBody.createFixture(environmentShape, 0);
+				environmentShape.dispose();
+			}
+			{ // bottom right house
+				ChainShape environmentShape = new ChainShape();
+				
+
+				Vector2[] vertices = vectorize(
+						new float[]{2945,2945,2974,3103,3136,3136,3042,3042},
+						new float[]{1122,1301,1310,1310,1301,1090,1090,1121});
 	
 				environmentShape.createLoop(vertices);
 				groundBody.createFixture(environmentShape, 0);
@@ -475,9 +644,13 @@ public class ZombieLord implements ApplicationListener {
 			posy = jumper.getPosition().y*this.PIXELS_PER_METER;
 			princess.setX(posx-32);
 			princess.setY(posy-10);
+			pointer.setTransform(jumper.getPosition().x, jumper.getPosition().y, 0);
 			
 			camera.position.x = princess.getX()+32;
 			camera.position.y = princess.getY()+16;
+			
+			// TODO: check if player has entered some interesting area
+			
 			
 			if(Gdx.input.isKeyPressed(Keys.B)){
 				System.out.println("position: x="+posx+", y="+posy);
@@ -551,7 +724,7 @@ public class ZombieLord implements ApplicationListener {
 			}
 		}
 		
-		boolean waiting = false; //TODO: move this and use this
+		boolean waiting = false; //TODO: move this?
 		
 		if(waitTime > 0)
 		{
@@ -743,10 +916,11 @@ public class ZombieLord implements ApplicationListener {
 		 s.draw(batch);
 		batch.end();
 		
-		/*debugRenderer.render(world, camera.combined.scale(
-				PIXELS_PER_METER,
-				PIXELS_PER_METER,
-				PIXELS_PER_METER));*/
+		if(world != null && debugRenderer != null)
+			debugRenderer.render(world, camera.combined.scale(
+					PIXELS_PER_METER,
+					PIXELS_PER_METER,
+					PIXELS_PER_METER));
 	}
 
 	@Override
